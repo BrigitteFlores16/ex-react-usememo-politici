0cdb2a979d63001f509cdfc745a24b74fd797e6e
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2025-05-09 00:24:03 +0200

    Milestone 1, 2 e 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
-import { useState } from "react";
+import { useState, useMemo, memo } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+function PoliticianList() {
+  const [politicians, setPoliticians] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const loadUsers = async () => {
+    const res = await fetch("http://localhost:5000/politicians");
+    const data = await res.json();
+    setPoliticians(data);
+  };
+
+  const filteredPoliticians = useMemo(() => {
+    return politicians.filter(
+      (politician) =>
+        politician.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        politician.biography.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }, [politicians, searchTerm]);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div>
+      <button onClick={loadUsers}>Carica Politici</button>
+      <input
+        type="text"
+        placeholder="Cerca un politico..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {filteredPoliticians.map((politician) => (
+        <Card key={politician.id} politician={politician} />
+      ))}
+    </div>
   );
 }
+const Card = memo(
+  ({ politician }) => {
+    console.log(`Rendering ${politician.name}`);
 
-export default App;
+    return (
+      <div className="card">
+        <h2>{politician.name}</h2>
+        <img src={politician.image} alt={politician.name} />
+        <h3>{politician.position}</h3>
+        <p>{politician.biography}</p>
+      </div>
+    );
+  },
+  (prevProps, nextProps) => prevProps.politician === nextProps.politician
+);
+
+export default PoliticianList;
